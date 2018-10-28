a667dea5a01fa7f6fe6d08c1a59943f9520d4679
unknown <renhaojie520@live.cn>
2018-10-29 07:47:29 +0800

    CustomShape

--- src/main.js
+++ src/main.js
@@ -1,27 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import 'normalize.css';
+import Vue from "vue";
+import App from "./App";
+import "normalize.css";
 Vue.config.productionTip = false;
-window.$ = require('jquery');
-window.Backbone = require('backbone');
+window.$ = require("jquery");
+window.Backbone = require("backbone");
 Backbone.$ = $;
-import 'jointjs/dist/joint.css';
-window.joint = require('jointjs');
-import { extendTextEditor } from './rappid/TextEditor';
-import { extendSelection } from './rappid/Selection';
-import { extendHalo } from './rappid/Halo';
-import { extendFreeTransform } from './rappid/FreeTransform';
-import { defineDefaultLink } from './rappid/Link';
-import { extendCommandManager } from './rappid/CommandManager';
+import "jointjs/dist/joint.css";
+window.joint = require("jointjs");
+import { extendTextEditor } from "./rappid/TextEditor";
+import { extendSelection } from "./rappid/Selection";
+import { extendHalo } from "./rappid/Halo";
+import { extendFreeTransform } from "./rappid/FreeTransform";
+import { defineDefaultLink } from "./rappid/Link";
+import { extendCommandManager } from "./rappid/CommandManager";
 extendTextEditor();
 extendSelection();
 extendHalo();
 extendFreeTransform();
 defineDefaultLink();
 extendCommandManager();
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { extendCustomNamespace } from "./shapes/custom";
+import { extendShapeCloud } from "./shapes/Cloud";
+extendCustomNamespace();
+extendShapeCloud();
+import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faSearchPlus,
   faSearchMinus,
@@ -31,8 +35,8 @@ import {
   faCaretRight,
   faFileImport,
   faFileExport
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(
   faSearchPlus,
@@ -44,11 +48,11 @@ library.add(
   faFileImport,
   faFileExport
 );
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   components: { App },
-  template: '<App/>'
+  template: "<App/>"
 });
---
+++ src/shapes/custom.js
@@ -0,0 +1,3 @@
+export function extendCustomNamespace() {
+  joint.shapes.custom = {};
+}
