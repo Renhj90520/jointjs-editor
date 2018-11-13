c858993f3ae58af45868d3ff9c42ce4ce1ff4ed8
任浩杰 <renhaojie@sysware.com.cn>
2018-11-13 18:29:21 +0800

    temp save

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -50,7 +50,18 @@ export default {
         path: {
           d:
             'M384 112v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h80c0-35.29 28.71-64 64-64s64 28.71 64 64h80c26.51 0 48 21.49 48 48zM192 40c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24m96 114v-20a6 6 0 0 0-6-6H102a6 6 0 0 0-6 6v20a6 6 0 0 0 6 6h180a6 6 0 0 0 6-6z',
-          fill: 'black'
+          fill: {
+                type:'linearGradient',
+                stops:[
+                  {offset:'20%',color:'#39F'},
+                  {offset:'90%',color:'#F3F'},
+                ]
+              },
+              'stroke-width':0,
+               filter: {
+                  name: "dropShadow",
+                  args: { dx: 1, dy: 2, blur: 8, color: "#000", opacity: 0.1 }
+                },
         },
         text: {
           ref: 'path',
